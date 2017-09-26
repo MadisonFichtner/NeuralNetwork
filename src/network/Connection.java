2f04c3f7161811fe71fd3a4c4b75406cc9668139
Nate Tranel <njtranel@gmail.com>
2017-09-25 18:24:37 -0600

    adds main class, constructors

--- src/network/Connection.java
+++ src/network/Connection.java
@@ -1,5 +1,21 @@
 package network;
 
 public class Connection {
-
+	private Neuron from, to;
+	private double weight;
+	
+	public Connection(Neuron from, Neuron to, double weight) {
+		this.from = from;
+		this.to = to;
+		this.weight = weight;
+	}
+	public Neuron getFrom() {
+		return from;
+	}
+	public Neuron getTo() {
+		return to;
+	}
+	public double getWeight() {
+		return weight;
+	}
 }
--- src/network/Network.java
+++ src/network/Network.java
@@ -1,5 +1,11 @@
 package network;
 
 public class Network {
-
+	private int hidLayer;
+	public Network(int inputs, int hidLayer, int hidNode, int outputs) {	//constructor for MLF network
+		this.hidLayer = hidLayer;
+	}
+	public Network(int inputs, int gauss, int outputs) {					//constructor for RBF network
+		
+	}
 }
