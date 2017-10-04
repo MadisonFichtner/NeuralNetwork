db0ce8a6885bf9dca3671b4f57b8476c2caef42f
Bryan Plant <bryanplant@gmail.com>
2017-10-03 18:53:26 -0600

    Network constructors add connections between layers Info about network is printed after creating network

--- src/network/Layer.java
+++ src/network/Layer.java
@@ -3,11 +3,11 @@ package network;
 import java.util.ArrayList;
 
 public class Layer {
-	ArrayList<Neuron> nodes;
-	int numNodes;
-	int nodeType;
-	int actFun;
-	
+	private ArrayList<Neuron> neurons;
+	private int numNodes;
+	private int nodeType;
+	private int actFun;
+
 	/*
 	 * creates new layer
 	 * @param numNodes: number of nodes in the layer
@@ -18,16 +18,28 @@ public class Layer {
 		this.numNodes = numNodes;
 		this.nodeType = nodeType;
 		this.actFun = actFun;
-		nodes = new ArrayList<Neuron>();
-		
+		neurons = new ArrayList<Neuron>();
+
 		//add nodes to layer
 		for(int i = 0; i < numNodes; i++){
-			addNode(new Neuron(nodeType, actFun));
+			addNeuron(new Neuron(nodeType, actFun));
 		}
 	}
-	
-	public void addNode(Neuron n) {
-		nodes.add(n);
+
+	public void addNeuron(Neuron n) {
+		neurons.add(n);
 	}
 
+	public Neuron getNeuron(int index){
+		return neurons.get(index);
+	}
+
+	//prints out information about layer
+	public void printLayer(int num){
+		System.out.println("Layer " + num + ":");
+		for(Neuron n : neurons){
+			n.printNeuron(neurons.indexOf(n)+1);
+		}
+		System.out.println();
+	}
 }
--- src/network/Network.java
+++ src/network/Network.java
@@ -6,29 +6,55 @@ public class Network {
 	private Layer inLayer;
 	private ArrayList<Layer> hidLayers;
 	private Layer outLayer;
-	
+	private int type;	//what type of network it is
+
 	/*
-	 * Create a MLP network
+	 * Create an MLP network
 	 * @param numInputs: number of input nodes
 	 * @param numHidLayers: number of hidden layers
 	 * @param numHidNodes: number of nodes in hidden layers
 	 * @param numOutputs: number of output nodes
-	 * @param actFun: type of activation function for nodes 
+	 * @param actFun: type of activation function for nodes
 	 */
 	public Network(int numInputs, int numHidLayers, int numHidNodes, int numOutputs, int actFun) {
 		//create input layer with inputs number of nodes, node type of 0, and no activation function
-		inLayer = new Layer(numInputs, 0, 0);	
-		
+		inLayer = new Layer(numInputs, 0, 0);
+
 		//create hidden layers with hidNode number of nodes, node type of 0, and given activation function
 		hidLayers = new ArrayList<Layer>();
 		for(int i = 0; i < numHidLayers; i++){
 			hidLayers.add(new Layer(numHidNodes, 0, actFun));
 		}
-		
+
 		//create output layer with outputs number of nodes, node type of 0, and linear activation function
-		outLayer = new Layer(numOutputs, 0, 1);									
+		outLayer = new Layer(numOutputs, 0, 1);
+
+		//add connections from input layer to first hid layer with initial weights of 1
+		for(int i = 0; i < numInputs; i++){
+			for(int j = 0; j < numHidNodes; j++){
+				inLayer.getNeuron(i).addConnection(hidLayers.get(0).getNeuron(j), 1);
+			}
+		}
+
+		//add connections from each hidden layer to the next hidden layer with initial weights of 1
+		for(int i = 0; i < numHidLayers-1; i ++){
+			for(int j = 0; j < numHidNodes; j++){
+				for(int k = 0; k < numHidLayers; k++){
+					hidLayers.get(i).getNeuron(j).addConnection(hidLayers.get(i+1).getNeuron(k), 1);
+				}
+			}
+		}
+
+		//add connections from last hidden layer to output layer with initial weights of 1
+		for(int i = 0; i < numHidNodes; i ++){
+			for(int j = 0; j < numOutputs; j++){
+				hidLayers.get(hidLayers.size()-1).getNeuron(i).addConnection(outLayer.getNeuron(j), 1);
+			}
+		}
+
+		type = 1;
 	}
-	
+
 	/*
 	 * Create an RBF network
 	 * @param numInputs: number of input nodes
@@ -37,18 +63,63 @@ public class Network {
 	 */
 	public Network(int numInputs, int numGaussians, int numOutputs) {					//constructor for RBF network
 		//create input layer with inputs number of nodes, node type of 0, and no activation function
-		inLayer = new Layer(numInputs, 0, 0);	
-		
+		inLayer = new Layer(numInputs, 0, 0);
+
 		//create hidden layer with gauss number of nodes, node type of 1, and no activation function
 		hidLayers = new ArrayList<Layer>();
 		hidLayers.add(new Layer(numGaussians, 1, 0));
-		
+
 		//create output layer with outputs number of nodes, node type of 0, and linear activation function
 		outLayer = new Layer(numOutputs, 0, 1);
+
+		//add connections from input layer to Gaussian layer with weights of 1
+		for(int i = 0; i < numInputs; i++){
+			for(int j = 0; j < numGaussians; j++){
+				inLayer.getNeuron(i).addConnection(hidLayers.get(0).getNeuron(j), 1);
+			}
+		}
+
+		//add connections from Gaussian layer to output layer with initial weights of 1
+		for(int i = 0; i < numGaussians; i ++){
+			for(int j = 0; j < numOutputs; j++){
+				hidLayers.get(0).getNeuron(i).addConnection(outLayer.getNeuron(j), 1);
+			}
+		}
+
+		type = 2;
 	}
-	
-	public void backprop() {
-		
+
+	public void backprop(){
+		//adjust all weights for MLP
+		//adjust only the weights between gaussian layer and output layer
+		if(type == 1){
+
+		}
+		else if(type == 2){
+
+		}
 	}
-	
+
+	public void train(){
+		// 1. It should run the inputs through the network and get an output
+		// 2. Then call calcError() with the output and the desired output to calculate error
+		// 3. Then call backprop() with the value of the error
+		// 4. Repeat with different input and output values
+		if(type == 1){
+
+		}
+		else if(type == 2){
+
+		}
+	}
+
+	//prints out information about network
+	public void printNetwork(){
+		inLayer.printLayer(1);
+		for(Layer l : hidLayers){
+			l.printLayer(hidLayers.indexOf(l) + 2);
+		}
+		outLayer.printLayer(2 + hidLayers.size());
+	}
+
 }
